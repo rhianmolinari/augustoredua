4b49e92a4c8ec7da1df3db14a627dae73a96a14b
Rhian Molinari <hello@rhianmolinari.com>
2017-09-18 22:08:03 -0300

    Teste

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
 
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-<head>
+<head> 
   <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
